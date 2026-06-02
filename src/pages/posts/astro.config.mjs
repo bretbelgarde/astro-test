6542ddc04d468361add930c769038d3d0c4950f4
Bret Belgarde <bretbelgarde@gmail.com>
2026-06-02 15:31:03 -0400

    install preact and add Greeting Component

--- src/pages/posts/astro.config.mjs
+++ src/pages/posts/astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://illustrious-selkie-8e1d9f.netlify.app/",
   integrations: [preact()]
 });
